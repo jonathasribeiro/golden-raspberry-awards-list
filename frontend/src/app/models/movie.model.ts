57bab8f9a072279d22e0893981cdec4e4a6660fe
jonathasribeiro <jonathasribeiroreal@gmail.com>
2025-08-06 17:12:41 -0300

    feat: finish list page

---
+++ frontend/src/app/models/movie.model.ts
@@ -0,0 +1,6 @@
+export interface Movie {
+  id: number;
+  year: number;
+  title: string;
+  winner: boolean;
+}
--- frontend/src/app/pages/movies/movies.component.ts
+++ frontend/src/app/pages/movies/movies.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MoviesService } from '../../services/movies.service';
+import { Movie } from '../../models/movie.model';
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './movies.component.html',
-  styleUrl: './movies.component.css'
 })
 export class MoviesComponent {
+  private movieService = inject(MoviesService);
 
+  movies: Movie[] = [];
+  yearFilter?: number;
+  winnerFilter?: boolean;
+  currentPage = 0;
+  totalPages = 0;
+  loading: boolean = false;
+
+  ngOnInit() {
+    this.fetchMovies();
+  }
+
+  fetchMovies() {
+    this.loading = true;
+    this.movieService.filterMovies(this.yearFilter, this.winnerFilter, this.currentPage).subscribe(res => {
+      this.movies = res.content;
+      this.totalPages = res.totalPages;
+      this.loading = false;
+    });
+  }
+
+  onSearch() {
+    this.currentPage = 0;
+    this.fetchMovies();
+  }
+
+  goToPage(page: number) {
+    if (page >= 0 && page < this.totalPages) {
+      this.currentPage = page;
+      this.fetchMovies();
+    }
+  }
 }
